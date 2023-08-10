2dfc487f3881a2519c4bf3ca0c527f54e9952d4f
sdtMurilloMelo <murillomelo@discente.ufg.br>
2023-08-10 15:51:47 -0300

    updating

--- main.c
+++ main.c
@@ -7,15 +7,7 @@
 // gcc -o main main.c -I/mingw64/include/SDL2 -L/mingw64/lib -lmingw32 -lSDL2main -lSDL2
 // gcc -o main main.c -lSDL2
 
-void drawPseudoPixel(SDL_Renderer *renderer, int x, int y, int r, int g, int b, int size)
-{
-    // Set the color of the square
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
 
-    // Draw the square
-    SDL_Rect squareRect = {x * size, y * size, size, size};
-    SDL_RenderFillRect(renderer, &squareRect);
-}
 
 int main(int argc, char *argv[])
 {
@@ -28,204 +20,15 @@ int main(int argc, char *argv[])
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // Maze parameters
-    unsigned int MazeWidth = 20, MazeHeight = 20, NumVisitedCells, PathWidth = 3;
-    unsigned int *m_cells;
+    unsigned int *m_cells, MazeWidth = 20, MazeHeight = 20, PathWidth = 3;
     m_cells = (unsigned int *)calloc(MazeWidth * MazeHeight, sizeof(unsigned int));
-    MazeStack m_stack;
-    init_MazeStack(&m_stack, MazeWidth * MazeHeight);
-    srand(time(NULL));
-    int x = rand() % MazeWidth, y = rand() % MazeHeight;
-    maze_stack_push(&m_stack, x, y);
-    m_cells[y * MazeWidth + x] = CELL_VISITED;
-    NumVisitedCells = 1;
-
-    // Create Maze
-    int last_cell_dir, count_repeated__cell_dir = 0, max_repeated_cell_dir;
-    while (NumVisitedCells < MazeWidth * MazeHeight)
-    {
-        // Create a set of unvisted neighbours
-        NeighboursStack neighbours;
-        init_NeighboursStack(&neighbours);
-        // North neighbour
-        if (m_stack.array[m_stack.top].y > 0 && (m_cells[offset(0, -1)] & CELL_VISITED) == 0)
-            neighbours_stack_push(&neighbours, 0);
-        // East neighbour
-        if (m_stack.array[m_stack.top].x < MazeWidth - 1 && (m_cells[offset(1, 0)] & CELL_VISITED) == 0)
-            neighbours_stack_push(&neighbours, 1);
-        // South neighbour
-        if (m_stack.array[m_stack.top].y < MazeHeight - 1 && (m_cells[offset(0, 1)] & CELL_VISITED) == 0)
-            neighbours_stack_push(&neighbours, 2);
-        // West neighbour
-        if (m_stack.array[m_stack.top].x > 0 && (m_cells[offset(-1, 0)] & CELL_VISITED) == 0)
-            neighbours_stack_push(&neighbours, 3);
-
-        // Are there any neighbours available?
-        if (neighbours.top != -1)
-        {
-            // Choose one available neighbour at random
-            srand(time(NULL)); // Seed the random number generator with the current time
-            int next_cell_dir = neighbours.array[rand() % (neighbours.top + 1)];
-            max_repeated_cell_dir = rand() % ((MazeWidth/3 < MazeHeight/3) ? MazeWidth/3 : MazeHeight/3);
-
-            if (next_cell_dir == last_cell_dir)
-            {
-                count_repeated__cell_dir++;
-            }
-            if ((count_repeated__cell_dir >= max_repeated_cell_dir && neighbours.top > 0) || (rand() % 4 == 0 && neighbours.top > 0))
-            {
-                do
-                {
-                    next_cell_dir = neighbours.array[rand() % (neighbours.top + 1)];
-                } while (next_cell_dir == last_cell_dir);
-
-                count_repeated__cell_dir = 0;
-            }
-
-            switch (next_cell_dir)
-            {
-            case 0: // North
-                m_cells[offset(0, -1)] |= CELL_VISITED | CELL_PATH_S;
-                m_cells[offset(0, 0)] |= CELL_PATH_N;
-                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 0, m_stack.array[m_stack.top].y - 1);
-                break;
-
-            case 1: // East
-                m_cells[offset(+1, 0)] |= CELL_VISITED | CELL_PATH_W;
-                m_cells[offset(0, 0)] |= CELL_PATH_E;
-                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 1, m_stack.array[m_stack.top].y + 0);
-                break;
-
-            case 2: // South
-                m_cells[offset(0, +1)] |= CELL_VISITED | CELL_PATH_N;
-                m_cells[offset(0, 0)] |= CELL_PATH_S;
-                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 0, m_stack.array[m_stack.top].y + 1);
-                break;
-
-            case 3: // West
-                m_cells[offset(-1, 0)] |= CELL_VISITED | CELL_PATH_E;
-                m_cells[offset(0, 0)] |= CELL_PATH_W;
-                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x - 1, m_stack.array[m_stack.top].y + 0);
-                break;
-            }
-
-            last_cell_dir = next_cell_dir;
-
-            NumVisitedCells++;
-        }
-        else
-        {
-            // No available neighbours so backtrack
-            maze_stack_pop(&m_stack);
-        }
-    }
-    for (int x = 1; x < MazeWidth - 1; x++)
-    {
-        for (int y = 1; y < MazeHeight - 1; y++)
-        {
-            srand(time(NULL));
-            int do_it = rand() % 5;
-            if (do_it == 0)
-            {
-                int dir = rand() % 4;
-                switch (dir)
-                {
-                case 0:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_N;
-                    m_cells[(y-1) * MazeWidth + x] |= CELL_PATH_S;
-                    break;
-                case 1:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_E;
-                    m_cells[y * MazeWidth + (x+1)] |= CELL_PATH_W;
-                    break;
-                case 2:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_S;
-                    m_cells[(y+1) * MazeWidth + x] |= CELL_PATH_N;
-                    break;
-                case 3:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_W;
-                    m_cells[y * MazeWidth + (x-1)] |= CELL_PATH_E;
-                    break;
-                default:
-                    break;
-                }
-                dir = rand() % 4;
-                switch (dir)
-                {
-                case 0:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_N;
-                    m_cells[(y-1) * MazeWidth + x] |= CELL_PATH_S;
-                    break;
-                case 1:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_E;
-                    m_cells[y * MazeWidth + (x+1)] |= CELL_PATH_W;
-                    break;
-                case 2:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_S;
-                    m_cells[(y+1) * MazeWidth + x] |= CELL_PATH_N;
-                    break;
-                case 3:
-                    m_cells[y * MazeWidth + x] |= CELL_PATH_W;
-                    m_cells[y * MazeWidth + (x-1)] |= CELL_PATH_E;
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
-    }
-    m_cells[0] |= CELL_PATH_E;
-    m_cells[m_cells[0 * MazeWidth + 1]] |= CELL_PATH_W;
-    m_cells[0] |= CELL_PATH_S;
-    m_cells[m_cells[1 * MazeWidth + 0]] |= CELL_PATH_N;
+    
+    // Create maze
+    create_maze(m_cells, MazeWidth, MazeHeight);
 
     // Draw maze
-    for (int x = 0; x < MazeWidth; x++)
-    {
-        for (int y = 0; y < MazeHeight; y++)
-        {
-            for (int px = 0; px < PathWidth; px++)
-            {
-                for (int py = 0; py < PathWidth; py++)
-                {
-                    if (m_cells[y * MazeWidth + x] & CELL_VISITED)
-                        drawPseudoPixel(renderer, x * (PathWidth + 1) + px, y * (PathWidth + 1) + py, 255, 255, 255, 6);
-                    else
-                        drawPseudoPixel(renderer, x * (PathWidth + 1) + px, y * (PathWidth + 1) + py, 255, 255, 255, 6);
-                }
-            }
-
-            for (int p = 0; p < PathWidth; p++)
-            {
-                if (m_cells[y * MazeWidth + x] & CELL_PATH_S)
-                    drawPseudoPixel(renderer, x * (PathWidth + 1) + p, y * (PathWidth + 1) + PathWidth, 255, 255, 255, 6); // Draw South Passage
-                if (m_cells[y * MazeWidth + x] & CELL_PATH_E)
-                    drawPseudoPixel(renderer, x * (PathWidth + 1) + PathWidth, y * (PathWidth + 1) + p, 255, 255, 255, 6); // Draw East Passage
-            }
-        }
-    }
-    for (int px = 0; px < PathWidth; px++)
-    {
-        for (int py = 0; py < PathWidth; py++)
-        {
-            drawPseudoPixel(renderer, 0 * (PathWidth + 1) + px, 0 * (PathWidth + 1) + py, 255, 0, 0, 6);
-        }
-    }
-    for (int px = 0; px < PathWidth; px++)
-    {
-        for (int py = 0; py < PathWidth; py++)
-        {
-            drawPseudoPixel(renderer, (MazeWidth - 1) * (PathWidth + 1) + px, (MazeHeight - 1) * (PathWidth + 1) + py, 0, 255, 0, 6);
-        }
-    }
-    for (int px = 0; px < PathWidth; px++)
-    {
-        for (int py = 0; py < PathWidth; py++)
-        {
-            drawPseudoPixel(renderer, (MazeWidth)/2 * (PathWidth + 1) + px, (MazeHeight)/2 * (PathWidth + 1) + py, 255, 255, 0, 6);
-        }
-    }
-    SDL_RenderPresent(renderer);
-
+    draw_maze(renderer, m_cells, MazeWidth, MazeHeight, PathWidth);
+    
     int running = 1;
     SDL_Event event;
     int x_position = 0, y_position = 0;
@@ -243,7 +46,6 @@ int main(int argc, char *argv[])
                 switch (event.key.keysym.sym)
                 {
                 case SDLK_UP:
-                    printf("Up arrow key was pressed.\n");
                     if (!(y_position == 0) && (m_cells[y_position * MazeWidth + x_position] & CELL_PATH_N))
                     {
                         for (int px = 0; px < PathWidth; px++)
@@ -265,7 +67,6 @@ int main(int argc, char *argv[])
                     }
                     break;
                 case SDLK_DOWN:
-                    printf("Down arrow key was pressed.\n");
                     if (!(y_position == MazeHeight - 1) && (m_cells[y_position * MazeWidth + x_position] & CELL_PATH_S))
                     {
                         for (int px = 0; px < PathWidth; px++)
@@ -287,7 +88,6 @@ int main(int argc, char *argv[])
                     }
                     break;
                 case SDLK_LEFT:
-                    printf("Left arrow key was pressed.\n");
                     if (!(x_position == 0) && (m_cells[y_position * MazeWidth + x_position] & CELL_PATH_W))
                     {
                         for (int px = 0; px < PathWidth; px++)
@@ -309,7 +109,6 @@ int main(int argc, char *argv[])
                     }
                     break;
                 case SDLK_RIGHT:
-                    printf("Right arrow key was pressed.\n");
                     if (!(x_position == MazeWidth - 1) && (m_cells[y_position * MazeWidth + x_position] & CELL_PATH_E))
                     {
                         for (int px = 0; px < PathWidth; px++)
@@ -331,36 +130,11 @@ int main(int argc, char *argv[])
                     }
                     break;
                 case SDLK_SPACE:
-                    printf("Space key was pressed.\n");
                     break;
                 default:
                     break;
                 }
             }
-            /* else if (event.type == SDL_KEYUP)
-            {
-                // Check the specific key that was released
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_UP:
-                    printf("Up arrow key was released.\n");
-                    break;
-                case SDLK_DOWN:
-                    printf("Down arrow key was released.\n");
-                    break;
-                case SDLK_LEFT:
-                    printf("Left arrow key was released.\n");
-                    break;
-                case SDLK_RIGHT:
-                    printf("Right arrow key was released.\n");
-                    break;
-                case SDLK_SPACE:
-                    printf("Space key was released.\n");
-                    break;
-                default:
-                    break;
-                }
-            } */
         }
     }
 
--- maze.h
+++ maze.h
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <time.h>
+#include <SDL2/SDL.h>
 
-#define offset(x0, y0) (m_stack.array[m_stack.top].y + y0) * MazeWidth + (m_stack.array[m_stack.top].x + x0)
+#define offset(x0, y0) (m_stack.array[m_stack.top].y + y0) * Width + (m_stack.array[m_stack.top].x + x0)
 
 enum
 {
@@ -15,29 +17,28 @@ typedef struct cell_pair
 {
     unsigned int x;
     unsigned int y;
-}CellPair;
+} CellPair;
 
 typedef struct maze_stack
 {
     int top;
     CellPair *array;
-}MazeStack;
+} MazeStack;
 
-
-void init_MazeStack(MazeStack* stack, unsigned int stack_size)
+void init_MazeStack(MazeStack *stack, unsigned int stack_size)
 {
     stack->top = -1;
-    stack->array = (CellPair*)calloc(stack_size, sizeof(CellPair));
+    stack->array = (CellPair *)calloc(stack_size, sizeof(CellPair));
 }
 
-void maze_stack_push(MazeStack* stack, unsigned int x0, unsigned int y0)
+void maze_stack_push(MazeStack *stack, unsigned int x0, unsigned int y0)
 {
     stack->top++;
     (stack->array[stack->top]).x = x0;
     (stack->array[stack->top]).y = y0;
 }
 
-void maze_stack_pop(MazeStack* stack)
+void maze_stack_pop(MazeStack *stack)
 {
     stack->top--;
 }
@@ -46,20 +47,173 @@ typedef struct maze_neighbours_stack
 {
     int top;
     unsigned int array[4];
-}NeighboursStack;
+} NeighboursStack;
 
-void init_NeighboursStack(NeighboursStack* stack)
+void init_NeighboursStack(NeighboursStack *stack)
 {
     stack->top = -1;
 }
 
-void neighbours_stack_push(NeighboursStack* stack, unsigned int element)
+void neighbours_stack_push(NeighboursStack *stack, unsigned int element)
 {
     stack->top++;
     stack->array[stack->top] = element;
 }
 
-void neighbours_stack_pop(NeighboursStack* stack)
+void neighbours_stack_pop(NeighboursStack *stack)
 {
     stack->top--;
+}
+
+void create_maze(unsigned int *cells, unsigned int Width, unsigned int Height)
+{
+    unsigned int NumVisitedCells;
+    MazeStack m_stack;
+    init_MazeStack(&m_stack, Width * Height);
+    
+    srand(time(NULL));
+    int x = rand() % Width, y = rand() % Height;
+    
+    maze_stack_push(&m_stack, x, y);
+    cells[y * Width + x] = CELL_VISITED;
+    NumVisitedCells = 1;
+
+    // Create Maze
+    int last_cell_dir, count_repeated__cell_dir = 0, max_repeated_cell_dir;
+    while (NumVisitedCells < Width * Height)
+    {
+        // Create a set of unvisted neighbours
+        NeighboursStack neighbours;
+        init_NeighboursStack(&neighbours);
+        // North neighbour
+        if (m_stack.array[m_stack.top].y > 0 && (cells[offset(0, -1)] & CELL_VISITED) == 0)
+            neighbours_stack_push(&neighbours, 0);
+        // East neighbour
+        if (m_stack.array[m_stack.top].x < Width - 1 && (cells[offset(1, 0)] & CELL_VISITED) == 0)
+            neighbours_stack_push(&neighbours, 1);
+        // South neighbour
+        if (m_stack.array[m_stack.top].y < Height - 1 && (cells[offset(0, 1)] & CELL_VISITED) == 0)
+            neighbours_stack_push(&neighbours, 2);
+        // West neighbour
+        if (m_stack.array[m_stack.top].x > 0 && (cells[offset(-1, 0)] & CELL_VISITED) == 0)
+            neighbours_stack_push(&neighbours, 3);
+
+        // Any neighbours available?
+        if (neighbours.top != -1)
+        {
+            // Choose one available neighbour at random
+            int next_cell_dir = neighbours.array[rand() % (neighbours.top + 1)];
+            max_repeated_cell_dir = rand() % ((Width / 3 < Height / 3) ? Width / 3 : Height / 3);
+
+            if (next_cell_dir == last_cell_dir)
+            {
+                count_repeated__cell_dir++;
+            }
+            if ((count_repeated__cell_dir >= max_repeated_cell_dir && neighbours.top > 0) || (rand() % 4 == 0 && neighbours.top > 0))
+            {
+                do
+                {
+                    next_cell_dir = neighbours.array[rand() % (neighbours.top + 1)];
+                } while (next_cell_dir == last_cell_dir);
+
+                count_repeated__cell_dir = 0;
+            }
+
+            switch (next_cell_dir)
+            {
+            case 0: // North
+                cells[offset(0, -1)] |= CELL_VISITED | CELL_PATH_S;
+                cells[offset(0, 0)] |= CELL_PATH_N;
+                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 0, m_stack.array[m_stack.top].y - 1);
+                break;
+
+            case 1: // East
+                cells[offset(+1, 0)] |= CELL_VISITED | CELL_PATH_W;
+                cells[offset(0, 0)] |= CELL_PATH_E;
+                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 1, m_stack.array[m_stack.top].y + 0);
+                break;
+
+            case 2: // South
+                cells[offset(0, +1)] |= CELL_VISITED | CELL_PATH_N;
+                cells[offset(0, 0)] |= CELL_PATH_S;
+                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x + 0, m_stack.array[m_stack.top].y + 1);
+                break;
+
+            case 3: // West
+                cells[offset(-1, 0)] |= CELL_VISITED | CELL_PATH_E;
+                cells[offset(0, 0)] |= CELL_PATH_W;
+                maze_stack_push(&m_stack, m_stack.array[m_stack.top].x - 1, m_stack.array[m_stack.top].y + 0);
+                break;
+            }
+
+            last_cell_dir = next_cell_dir;
+
+            NumVisitedCells++;
+        }
+        else
+        {
+            // No available neighbours so backtrack
+            maze_stack_pop(&m_stack);
+        }
+    }
+    for (int x = 0; x < Width; x++)
+    {
+        for (int y = 0; y < Height; y++)
+        {
+
+        }
+    }
+}
+
+void drawPseudoPixel(SDL_Renderer *renderer, int x, int y, int r, int g, int b, int size)
+{
+    // Set the color of the square
+    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+
+    // Draw the square
+    SDL_Rect squareRect = {x * size, y * size, size, size};
+    SDL_RenderFillRect(renderer, &squareRect);
+}
+
+void draw_maze(SDL_Renderer *renderer, unsigned int *cells, unsigned int Width, unsigned int Height, unsigned int PathWidth)
+{
+    for (int x = 0; x < Width; x++)
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            for (int px = 0; px < PathWidth; px++)
+            {
+                for (int py = 0; py < PathWidth; py++)
+                {
+                    if (cells[y * Width + x] & CELL_VISITED)
+                        drawPseudoPixel(renderer, x * (PathWidth + 1) + px, y * (PathWidth + 1) + py, 255, 255, 255, 6);
+                    else
+                        drawPseudoPixel(renderer, x * (PathWidth + 1) + px, y * (PathWidth + 1) + py, 255, 255, 255, 6);
+                }
+            }
+
+            for (int p = 0; p < PathWidth; p++)
+            {
+                if (cells[y * Width + x] & CELL_PATH_S)
+                    drawPseudoPixel(renderer, x * (PathWidth + 1) + p, y * (PathWidth + 1) + PathWidth, 255, 255, 255, 6); // Draw South Passage
+                if (cells[y * Width + x] & CELL_PATH_E)
+                    drawPseudoPixel(renderer, x * (PathWidth + 1) + PathWidth, y * (PathWidth + 1) + p, 255, 255, 255, 6); // Draw East Passage
+            }
+        }
+    }
+    for (int px = 0; px < PathWidth; px++)
+    {
+        for (int py = 0; py < PathWidth; py++)
+        {
+            drawPseudoPixel(renderer, 0 * (PathWidth + 1) + px, 0 * (PathWidth + 1) + py, 255, 0, 0, 6);
+        }
+    }
+    for (int px = 0; px < PathWidth; px++)
+    {
+        for (int py = 0; py < PathWidth; py++)
+        {
+            drawPseudoPixel(renderer, (Width - 1) * (PathWidth + 1) + px, (Height - 1) * (PathWidth + 1) + py, 0, 255, 0, 6);
+        }
+    }
+    SDL_RenderPresent(renderer);
 }
